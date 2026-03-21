f251139f88c3c42f24c5529ec49073106c12471f
Luiz Felipe Moureau <luizfelipesmoureau@gmail.com>
2026-03-21 04:56:32 -0300

    Remove benchmark package and related files

--- packages/benchmark/client.js
+++
@@ -1,5 +0,0 @@
-import { ApplicationExample } from "./src/Application";
-import { NewstackClient } from "@newstack/framework";
-
-const app = new ApplicationExample();
-new NewstackClient().start(app);
--- packages/benchmark/server.js
+++
@@ -1,5 +0,0 @@
-import { ApplicationExample } from "./src/Application";
-import { NewstackServer } from "@newstack/framework/server";
-
-const app = new ApplicationExample();
-new NewstackServer().start(app);
